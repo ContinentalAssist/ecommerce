a5211410edab8e951b963089d862037e9ba145f1
SamOmega <scontreras@omegaassist.com>
2025-09-03 19:09:43 -0500

    cambios en el llamado de clarity al partytown de qwik

--- src/root.tsx
+++ src/root.tsx
@@ -150,16 +150,18 @@ export default component$(() => {
     <QwikCityProvider>
     <head>
       <meta charset="utf-8" />
-      <QwikPartytown forward={['gtag', 'dataLayer.push', 'clarity']} />
+      <QwikPartytown forward={['gtag', 'dataLayer.push']} />
 	    <script id="cookieyes" type="text/javascript" src="https://cdn-cookieyes.com/client_data/3abc652a5abb2aba53a26980/script.js"></script>
       <script async src="https://www.googletagmanager.com/gtag/js?id=AW-11397008041" defer></script>
-      <script type="text/javascript" dangerouslySetInnerHTML={`
+      <script>
+        {`
         (function(c,l,a,r,i,t,y){
             c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
             t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
             y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
         })(window, document, "clarity", "script", "t4nob06eys");
-      `} />
+        `}
+      </script>
       <meta name="keywords" content="
               seguro, 
               seguro viajes,
