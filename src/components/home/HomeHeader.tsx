960024f6e06c79ca48db487f6711b850b540fad1
SamOmega <scontreras@continentalassist.com>
2026-01-06 16:06:28 -0500

    Reversion de los cambios de navidad, para volver a la publicidad del plan familiar

--- src/components/home/HomeHeader.tsx
+++ src/components/home/HomeHeader.tsx
@@ -14,26 +14,26 @@ export const HomeHeader = component$((props: HomeHeaderProps) => {
             <div class='container position-relative min-h-[75vh] md:min-h-[75vh] min-[0px]:min-h-[85vh] position-top-home'>
                 <div id='container-quote' class='row align-content-center justify-content-center'>
                     <div class='col-xl-12 col-lg-6 text-center home-magin pt-0 md:pt-12 mt-6 md:mt-6'>
-                        <div class="col-md-12 text-center d-none justify-content-center" id="collapseQuotesEngine-moblie">
+                        <div class="col-md-12 text-center d-none justify-content-center mb-4" id="collapseQuotesEngine-moblie">
                             <div>
-                                <h1 class="text-white">
-                                        Regálate un viaje <br />seguro <span
-                                        class="text-bold ">esta Navidad</span>
+                                <h1 class="text-blue">
+                                        Viaja con tranquilidad,<br /> 
+                                        <span class="text-bold">protegido</span>
+                                        en todo momento.
                                 </h1>
-                            </div>
-                        </div>
-                        <div class="col-12 text-center  justify-content-center">
-                            <div class="d-block d-md-none margin-header col-md-6 image-mujer-mobile" id="collapseQuotesEngine">
-                                <img src="https://evacotizacion.nyc3.cdn.digitaloceanspaces.com/imagenes/navidadImagenCupon.png" alt="mujer-header" class="img-fluid-mujer-BF" />
                             </div>
                         </div>
                         <div class="card card-body border-none shadow-lg qoutes-card">
                             <QuotesEngine modeResumeStep={props.modeResumeStep} headerStep={props.headerStep} />
                         </div>
+                        <div class="col-12 text-center  justify-content-center">
+                            <div class="d-block d-md-none margin-header col-md-6 image-mujer-mobile" id="collapseQuotesEngine">
+                                <img src="https://evacotizacion.nyc3.cdn.digitaloceanspaces.com/imagenes/imagen-header-familia_Eco.png" alt="mujer-header" class="img-fluid-mujer-BF" />
+                            </div>
+                        </div>
                         <div class="d-flex " id="banner-text-mobile">
 
                             <div class="collapse d-none margin-header col-md-6" id="collapseBtnQuotesEngine">
-
                                 <button type="button" class="btn btn-primary btn-lg mt-4" onClick$={() => {
                                     props.openQuotesEngine$(true)
                                 }}>¡Quiero comprar!
@@ -41,10 +41,11 @@ export const HomeHeader = component$((props: HomeHeaderProps) => {
                             </div>
                             <div class="collapse show mt-8 margin-header col-md-6" id="collapseQuotesEngine">
                                 <div>
-                                    <h1 class='text-while'>
-                                    Regálate un viaje<br />seguro <span
-                                            class="text-bold color-text-blue-home">esta Navidad</span>
-                                    </h1>
+                                    <h2 class='text-while'>
+                                    Viaja con tranquilidad, <br />
+                                    <span class="text-bold color-text-blue-home">protegido </span>
+                                    en todo momento.
+                                    </h2>
                                     <h2 class='h5 text-regular text-sub-while'>
                                         <span style={{ fontSize: '25px', marginRight: '13px' }}>⭐⭐⭐⭐⭐</span> Más de <span class='text-bold'>10 millones de viajeros </span> han
                                         confiado en nuestras asistencias en todo el mundo
@@ -56,7 +57,7 @@ export const HomeHeader = component$((props: HomeHeaderProps) => {
                 </div>
                 {/* Imagen MujerNavidad para Desktop - Dentro del container para alinearse con el layout */}
                 <div class="d-none d-md-block image-mujer" id="collapseQuotesEngine-desktop">
-                    <img src="https://evacotizacion.nyc3.cdn.digitaloceanspaces.com/imagenes/navidadImagenCuponDifuminado.png" alt="mujer-header" class="img-fluid-mujer-BF" />
+                    <img src="https://evacotizacion.nyc3.cdn.digitaloceanspaces.com/imagenes/imagen-header-familia_Eco.png" alt="mujer-header" class="img-fluid-mujer-BF" />
                 </div>
             </div>
         </div>
